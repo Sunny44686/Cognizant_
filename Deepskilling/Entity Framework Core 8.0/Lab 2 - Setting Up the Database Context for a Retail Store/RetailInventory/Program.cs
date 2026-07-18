f05fcc03bba1976c922c04feaf75a3e14ae3a16d
Sunny44686 <gubbavenkat623@gmail.com>
2026-07-18 15:51:07 +0530

    Create Program.cs

---
+++ Deepskilling/Entity Framework Core 8.0/Lab 2 - Setting Up the Database Context for a Retail Store/RetailInventory/Program.cs
@@ -0,0 +1,7 @@
+﻿using RetailInventory.Data;
+
+Console.WriteLine("Retail Inventory System");
+Console.WriteLine("Database Context Configured Successfully!");
+
+AppDbContext context = new AppDbContext();
+Console.ReadLine();
