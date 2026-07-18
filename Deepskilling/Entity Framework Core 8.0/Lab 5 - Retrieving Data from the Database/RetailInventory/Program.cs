d1a818119787842cba4494596b06e4a81b34f65e
Sunny44686 <gubbavenkat623@gmail.com>
2026-07-18 16:17:14 +0530

    Create Program.cs

---
+++ Deepskilling/Entity Framework Core 8.0/Lab 5 - Retrieving Data from the Database/RetailInventory/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RetailInventory.Data;
+
+using var context = new AppDbContext();
+
+Console.WriteLine("========== ALL PRODUCTS ==========");
+
+var products = await context.Products.ToListAsync();
+
+foreach (var p in products)
+{
+    Console.WriteLine($"{p.Name} - ₹{p.Price}");
+}
+
+Console.WriteLine();
+
+Console.WriteLine("========== FIND BY ID ==========");
+
+var product = await context.Products.FindAsync(1);
+
+Console.WriteLine($"Found: {product?.Name}");
+
+Console.WriteLine();
+
+Console.WriteLine("========== EXPENSIVE PRODUCT ==========");
+
+var expensive = await context.Products
+    .FirstOrDefaultAsync(p => p.Price > 50000);
+
+Console.WriteLine($"Expensive: {expensive?.Name}");
+
+Console.ReadLine();
