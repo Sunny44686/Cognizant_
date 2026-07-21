7a514c3007ef04ce9987532427fae27e3333ba74
Sunny44686 <gubbavenkat623@gmail.com>
2026-07-21 18:20:43 +0530

    Create UnitTest1.cs

---
+++ Deepskilling/NUnit and Moq/1. NUnit-Handson/NUnitHandson/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿namespace NUnitHandson
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            calculator = new Calculator();
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            calculator = null;
+        }
+
+        [Test]
+        [TestCase(10, 20, 30)]
+        [TestCase(5, 15, 20)]
+        [TestCase(100, 200, 300)]
+        public void TestAddition(int a, int b, int expected)
+        {
+            int actual = calculator.Add(a, b);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
