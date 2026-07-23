c3375cb419d1128f94fa342f555d324b40f1a70f
Sunny44686 <gubbavenkat623@gmail.com>
2026-07-23 20:54:45 +0530

    Create AdminController.cs

---
+++ Deepskilling/Microservices/JwtAuthenticationDemo/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthenticationDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AdminController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public IActionResult GetAdminData()
+        {
+            return Ok(new
+            {
+                Message = "Welcome Admin!",
+                Status = "Access Granted"
+            });
+        }
+    }
+}
