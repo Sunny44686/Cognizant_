6eb4e37e94f4cff75aa30376eff13c346e8aca59
Sunny44686 <gubbavenkat623@gmail.com>
2026-07-23 21:41:45 +0530

    Create CustomExceptionFilter.cs

---
+++ Deepskilling/WebApi/5. WebApi_Handson/FirstWebApi/Filters/CustomExceptionFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace FirstWebApi.Filters
+{
+    public class CustomExceptionFilter : IExceptionFilter
+    {
+        public void OnException(ExceptionContext context)
+        {
+            string logPath = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "ExceptionLog.txt");
+
+            File.AppendAllText(
+                logPath,
+                $"[{DateTime.Now}] {context.Exception}\n\n");
+
+            context.Result = new ObjectResult(new
+            {
+                Message = "Internal Server Error",
+                Error = context.Exception.Message
+            })
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+
+            context.ExceptionHandled = true;
+        }
+    }
+}
