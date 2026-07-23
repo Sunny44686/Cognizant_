79ef2d3f3bea512ae8958990165817694fd60730
Sunny44686 <gubbavenkat623@gmail.com>
2026-07-23 22:22:24 +0530

    Create KafkaController.cs

---
+++ Deepskilling/WebApi/6. WebApi_Handson/KafkaConsoleChat/Controllers/KafkaController.cs
@@ -0,0 +1,26 @@
+﻿using KafkaConsoleChat.Models;
+using KafkaConsoleChat.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KafkaConsoleChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KafkaController : ControllerBase
+    {
+        private readonly KafkaProducer producer;
+
+        public KafkaController(KafkaProducer producer)
+        {
+            this.producer = producer;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SendMessage(ChatMessage chat)
+        {
+            var result = await producer.SendMessage(chat.Message);
+
+            return Ok(result);
+        }
+    }
+}
