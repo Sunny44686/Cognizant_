642e8b3e3d5be0ba5ecba7976f0e5eb158e45119
Sunny44686 <gubbavenkat623@gmail.com>
2026-07-23 22:26:22 +0530

    Create KafkaProducer.cs

---
+++ Deepskilling/WebApi/6. WebApi_Handson/KafkaConsoleChat/Services/KafkaProducer.cs
@@ -0,0 +1,31 @@
+﻿using Confluent.Kafka;
+
+namespace KafkaConsoleChat.Services
+{
+    public class KafkaProducer
+    {
+        private readonly ProducerConfig config;
+
+        public KafkaProducer()
+        {
+            config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"
+            };
+        }
+
+        public async Task<string> SendMessage(string message)
+        {
+            using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+            var result = await producer.ProduceAsync(
+                "ChatTopic",
+                new Message<Null, string>
+                {
+                    Value = message
+                });
+
+            return $"Message sent successfully to {result.TopicPartitionOffset}";
+        }
+    }
+}
