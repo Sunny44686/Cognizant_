b919047ef5d4388933700cb621eb7ea757a4a56b
Sunny44686 <gubbavenkat623@gmail.com>
2026-07-23 20:55:50 +0530

    Create EmployeeController.cs

---
+++ Deepskilling/Microservices/JwtAuthenticationDemo/Controllers/EmployeeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthenticationDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetEmployees()
+        {
+            return Ok(new[]
+            {
+                "John",
+                "David",
+                "Alice",
+                "Robert"
+            });
+        }
+    }
+}
